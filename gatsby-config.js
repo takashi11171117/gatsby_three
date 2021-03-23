16db5ebf5640ec84cec4e9d5439393ffc16da145
Takashi Saito <takashi11171117@gmail.com>
2021-03-23 21:35:43 +0900

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "ThreeJs",
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <p>あああああ</p>
+    </main>
+  )
+}
+
+export default IndexPage
